9165f50fd3d072b2d228b28baa2e51334f5c769d
Kris <kris-lebedeva@bk.ru>
2022-07-24 21:19:31 +0300

    HomeWork1

---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿/* Задача 2
+Console.WriteLine("Input first integer number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input second integer number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+ 
+    if(num1 > num2)
+    {
+        Console.WriteLine("num1 > num2");
+    }
+    else 
+    if (num1 < num2)
+    {
+        Console.WriteLine("num1 < num2");
+    }
+    */
+
+
+/* Задача 4
+Console.WriteLine("Input first integer number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input second integer number: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input third integer number: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+    if(b > max)    max = b;
+    if(c > max)    max = c;
+
+Console.WriteLine(max);
+*/
+
+ /* Задача 6
+Console.WriteLine("Input integer number: ");
+int a = Convert.ToInt32(Console.ReadLine()); 
+ 
+    if (a % 2 == 0)
+    {
+        Console.WriteLine("even mumber");
+           
+    }
+     else
+    {
+        Console.WriteLine("odd number");
+    }
+    */
+
+/* Console.WriteLine("Input integer number: ");
+int N = Convert.ToInt32(Console.ReadLine()); 
+Console.WriteLine("Output ever number: ");
+int count = 1;
+while (count < N)
+    {
+        count++;
+        if (count % 2 == 0)
+    {
+        Console.WriteLine (count);
+    }
+        
+    }
+    */
+
+
+    
+    
